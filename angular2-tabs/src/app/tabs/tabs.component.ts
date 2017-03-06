b18029a8c5b0d3c4f3b432bf7be10f61ffc81ae5
Yue Gu <yuegu1993@gmail.com>
2017-03-06 00:41:50 -0800

    tab content

--- angular2-tabs/src/app/tabs/tabs.component.ts
+++ angular2-tabs/src/app/tabs/tabs.component.ts
@@ -3,15 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tabs',
   template: `
-    <ul>
-    <li *ngFor="let tab of tabs">{{tab.name}}</li>
-    </ul>
+    <div>
+      <ul>
+        <li *ngFor="let tab of tabs" class="tabs" id={{tabs.indexOf(tab)}}>{{tab.name}}</li>
+      </ul>
+      <div>
+        <p>
+        {{tabs[selected].content}}
+        </p>
+      </div>
+    </div>
   `,
-  styles: []
+  styles: [
+    `
+    .tabs{
+      list-style: none;
+    }
+    `
+  ]
 })
 export class TabsComponent implements OnInit {
   tabs = [{'name': 'tab 1', 'content': ' tab 1 content'}, {'name': 'tab 2', 'content': ' tab 2 content'},
           {'name': 'tab 3', 'content': ' tab 3 content'}];
+  selected = 0;
   constructor() {
   }
 
