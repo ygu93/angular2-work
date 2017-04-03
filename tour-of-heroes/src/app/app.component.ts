73982b75b1880ecb4e8cf20d3be56cb8bfb4a880
Yue Gu <yuegu1993@gmail.com>
2017-04-02 19:14:01 -0700

    move app component to heroes component

--- tour-of-heroes/src/app/app.component.ts
+++ tour-of-heroes/src/app/app.component.ts
@@ -1,30 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
 
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [HeroService]
+  template: `
+  <h1>
+    {{title}}
+  </h1>
+  <app-my-heroes></app-my-heroes>
+  `
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Tour of Heroes';
-  selectedHero: Hero;
-  heroes: Hero[];
 
-  constructor(private heroService: HeroService) { }
+  constructor() { }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-
-  ngOnInit(): void {
-    this.getHeroes();
-  }
 }
---
+++ tour-of-heroes/src/app/heroes.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+
+@Component({
+  selector: 'app-my-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  title = 'Tour of Heroes';
+  selectedHero: Hero;
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+}
